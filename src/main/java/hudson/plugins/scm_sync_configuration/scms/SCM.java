de82a9dbc33b876cef7430d28db32af0f27129a4
fcamblor <fcamblor@dev.java.net>
2010-10-11 22:14:43 +0000

    provided repository url help file

--- src/main/java/hudson/plugins/scm_sync_configuration/scms/SCM.java
+++ src/main/java/hudson/plugins/scm_sync_configuration/scms/SCM.java
@@ -4,7 +4,7 @@ import org.kohsuke.stapler.StaplerRequest;
 
 public enum SCM {
 	
-	SUBVERSION("Subversion", "svn-config.jelly"){
+	SUBVERSION("Subversion", "svn-config.jelly", "hudson.scm.SubversionSCM", "/plugins/scm_sync_configuration/ScmSyncConfigurationPlugin/SubversionSCM/scms/"){
 		private static final String SCM_URL_PREFIX="scm:svn:";
 		public String createScmUrlFromRequest(StaplerRequest req) {
 			return SCM_URL_PREFIX+req.getParameter("repositoryUrl");
@@ -12,17 +12,18 @@ public enum SCM {
 		public String extractScmUrlFrom(String scmUrl) {
 			return scmUrl.substring(SCM_URL_PREFIX.length());
 		}
-		public String getSCMDescriptorClassName() {
-			return "hudson.scm.SubversionSCM";
-		}
 	};
 
 	private String title;
 	private String configPage;
+	private String scmDescriptorClassName;
+	private String repositoryUrlHelpPath;
 	
-	private SCM(String _title, String _configPage){
+	private SCM(String _title, String _configPage, String _scmDescriptorClassName, String _repositoryUrlHelpPath){
 		this.title = _title;
 		this.configPage = _configPage;
+		this.scmDescriptorClassName = _scmDescriptorClassName;
+		this.repositoryUrlHelpPath = _repositoryUrlHelpPath;
 	}
 	
 	public String getTitle(){
@@ -32,8 +33,14 @@ public enum SCM {
 	public String getConfigPage(){
 		return this.configPage;
 	}
-	
+
+	public String getSCMDescriptorClassName() {
+		return this.scmDescriptorClassName;
+	}
+	public String getRepositoryUrlHelpPath() {
+		return this.repositoryUrlHelpPath;
+	}
+
 	public abstract String createScmUrlFromRequest(StaplerRequest req);
 	public abstract String extractScmUrlFrom(String scmUrl);
-	public abstract String getSCMDescriptorClassName();
 }
