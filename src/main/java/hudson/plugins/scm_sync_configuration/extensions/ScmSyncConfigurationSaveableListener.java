d9fafd955b78f24b564d6030104340d5571a6583
fcamblor <fcamblor@dev.java.net>
2010-10-18 19:58:04 +0000

    deleted useless logs

--- src/main/java/hudson/plugins/scm_sync_configuration/extensions/ScmSyncConfigurationSaveableListener.java
+++ src/main/java/hudson/plugins/scm_sync_configuration/extensions/ScmSyncConfigurationSaveableListener.java
@@ -3,13 +3,10 @@ package hudson.plugins.scm_sync_configuration.extensions;
 import hudson.Extension;
 import hudson.XmlFile;
 import hudson.model.Saveable;
-import hudson.model.Job;
 import hudson.model.listeners.SaveableListener;
-import hudson.plugins.scm_sync_configuration.HudsonFilesHelper;
 import hudson.plugins.scm_sync_configuration.ScmSyncConfigurationPlugin;
 import hudson.plugins.scm_sync_configuration.strategies.ScmSyncStrategy;
 
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @Extension
@@ -27,11 +24,6 @@ public class ScmSyncConfigurationSaveableListener extends SaveableListener{
 		
 		ScmSyncConfigurationPlugin plugin = ScmSyncConfigurationPlugin.getInstance();
 		ScmSyncStrategy strategy = plugin.getStrategyForSaveable(o, file.getFile());
-		
-		LOGGER.log(Level.FINEST, "Test log finest");
-		LOGGER.log(Level.INFO, "Saveable class : "+o.getClass()+", extends Job="+Job.class.isAssignableFrom(o.getClass()));
-		LOGGER.log(Level.INFO, "File : "+HudsonFilesHelper.buildPathRelativeToHudsonRoot(file.getFile()));
-		LOGGER.log(Level.INFO, "=> Result : "+String.valueOf(strategy));
 		
 		if(strategy != null){
 			plugin.commitFile(file);
