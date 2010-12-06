525e68424a42ec6ded924e641d20db22a1b9dff0
fcamblor <fcamblor@dev.java.net>
2010-12-06 21:16:21 +0000

    providing local svn repo + hudson hierarchy during unit tests

--- src/test/java/hudson/plugins/scm_sync_configuration/util/ScmSyncConfigurationBaseTest.java
+++ src/test/java/hudson/plugins/scm_sync_configuration/util/ScmSyncConfigurationBaseTest.java
@@ -7,37 +7,48 @@ import java.io.IOException;
 
 import hudson.model.Hudson;
 
+import org.codehaus.plexus.util.FileUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import static org.powermock.api.easymock.PowerMock.*;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+import org.springframework.core.io.ClassPathResource;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(Hudson.class)
 public class ScmSyncConfigurationBaseTest {
 	
-	private File tmpHudsonRootDir = null;
+	private File currentTestDirectory = null;
+	private File curentLocalSvnRepository = null;
 
 	@Before
 	public void setup() throws Throwable {
+		// Mocking Hudson singleton instance ...
 		mockStatic(Hudson.class);
 		Hudson hudsonMockedInstance = createPartialMock(Hudson.class, new String[]{ "getRootDir" });
 		expect(Hudson.getInstance()).andStubReturn(hudsonMockedInstance);
 		
-		tmpHudsonRootDir = createTmpDirectory();
-		expect(hudsonMockedInstance.getRootDir()).andStubReturn(tmpHudsonRootDir);
-		//expect(hudsonMockedInstance.getRootDir()).andThrow(new RuntimeException("aie !"));
+		// Mocking Hudson root directory
+		currentTestDirectory = createTmpDirectory();
+		File tmpHudsonRoot = new File(currentTestDirectory.getAbsolutePath()+"/hudsonRootDir/");
+	    if(!(tmpHudsonRoot.mkdir())) { throw new IOException("Could not create hudson root directory: " + tmpHudsonRoot.getAbsolutePath()); }
+		expect(hudsonMockedInstance.getRootDir()).andStubReturn(tmpHudsonRoot);
+		FileUtils.copyDirectoryStructure(new ClassPathResource("hudsonRootBaseTemplate/").getFile(), tmpHudsonRoot);
+		
+		// Creating local SVN repository...
+		File curentLocalSvnRepository = new File(currentTestDirectory.getAbsolutePath()+"/svnLocalRepo/");
+	    if(!(curentLocalSvnRepository.mkdir())) { throw new IOException("Could not create SVN local repo directory: " + curentLocalSvnRepository.getAbsolutePath()); }
+	    FileUtils.copyDirectoryStructure(new ClassPathResource("svnEmptyRepository").getFile(), curentLocalSvnRepository);
 		
 		replayAll();
 	}
 	
 	@After
 	public void teardown() throws Throwable {
-		// Deleting hudson root directory
-		tmpHudsonRootDir.delete();
-		
+		// Deleting current test directory
+		currentTestDirectory.delete();
 	}
 	
 	protected static File createTmpDirectory() throws IOException {
@@ -45,5 +56,13 @@ public class ScmSyncConfigurationBaseTest {
 	    if(!(temp.delete())) { throw new IOException("Could not delete temp file: " + temp.getAbsolutePath()); }
 	    if(!(temp.mkdir())) { throw new IOException("Could not create temp directory: " + temp.getAbsolutePath()); }
 	    return (temp);
+	}
+
+	protected File getCurrentTestDirectory() {
+		return currentTestDirectory;
+	}
+
+	protected File getCurentLocalSvnRepository() {
+		return curentLocalSvnRepository;
 	}
 }
