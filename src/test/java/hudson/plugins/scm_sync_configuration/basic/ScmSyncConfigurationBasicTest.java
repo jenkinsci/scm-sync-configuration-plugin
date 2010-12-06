46c9d615b1e0055457a3c9ce574370bb7fef8f50
fcamblor <fcamblor@dev.java.net>
2010-12-06 20:54:56 +0000

    provided rootDir mock on hudson singleton instance

--- src/test/java/hudson/plugins/scm_sync_configuration/basic/ScmSyncConfigurationBasicTest.java
+++ src/test/java/hudson/plugins/scm_sync_configuration/basic/ScmSyncConfigurationBasicTest.java
@@ -5,6 +5,9 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
+
+import java.io.File;
+
 import hudson.model.Hudson;
 import hudson.plugins.scm_sync_configuration.util.ScmSyncConfigurationBaseTest;
 
@@ -18,5 +21,14 @@ public class ScmSyncConfigurationBasicTest extends ScmSyncConfigurationBaseTest 
 		Hudson hudsonInstance = Hudson.getInstance();
 		assertThat(hudsonInstance, is(notNullValue()));
 		assertThat(hudsonInstance.toString().split("@")[0], is(not(equalTo("hudson.model.Hudson"))));
+	}
+	
+	@Test
+	public void shouldVerifyIfHudsonRootDirectoryExists() throws Throwable {
+		
+		Hudson hudsonInstance = Hudson.getInstance();
+		File hudsonRootDir = hudsonInstance.getRootDir();
+		assertThat(hudsonRootDir, is(not(equalTo(null))));
+		assert hudsonRootDir.exists();
 	}
 }
--- src/test/java/hudson/plugins/scm_sync_configuration/util/ScmSyncConfigurationBaseTest.java
+++ src/test/java/hudson/plugins/scm_sync_configuration/util/ScmSyncConfigurationBaseTest.java
@@ -1,8 +1,13 @@
 package hudson.plugins.scm_sync_configuration.util;
 
 import static org.easymock.EasyMock.expect;
+
+import java.io.File;
+import java.io.IOException;
+
 import hudson.model.Hudson;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import static org.powermock.api.easymock.PowerMock.*;
@@ -12,13 +17,33 @@ import org.powermock.modules.junit4.PowerMockRunner;
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(Hudson.class)
 public class ScmSyncConfigurationBaseTest {
+	
+	private File tmpHudsonRootDir = null;
 
 	@Before
 	public void setup() throws Throwable {
 		mockStatic(Hudson.class);
-		Hudson hudsonMockedInstance = createMock(Hudson.class);
+		Hudson hudsonMockedInstance = createPartialMock(Hudson.class, new String[]{ "getRootDir" });
 		expect(Hudson.getInstance()).andStubReturn(hudsonMockedInstance);
 		
-		replay(Hudson.class);
+		tmpHudsonRootDir = createTmpDirectory();
+		expect(hudsonMockedInstance.getRootDir()).andStubReturn(tmpHudsonRootDir);
+		//expect(hudsonMockedInstance.getRootDir()).andThrow(new RuntimeException("aie !"));
+		
+		replayAll();
+	}
+	
+	@After
+	public void teardown() throws Throwable {
+		// Deleting hudson root directory
+		tmpHudsonRootDir.delete();
+		
+	}
+	
+	protected static File createTmpDirectory() throws IOException {
+	    final File temp = File.createTempFile("hudsonHomeRoot", Long.toString(System.nanoTime()));
+	    if(!(temp.delete())) { throw new IOException("Could not delete temp file: " + temp.getAbsolutePath()); }
+	    if(!(temp.mkdir())) { throw new IOException("Could not create temp directory: " + temp.getAbsolutePath()); }
+	    return (temp);
 	}
 }
