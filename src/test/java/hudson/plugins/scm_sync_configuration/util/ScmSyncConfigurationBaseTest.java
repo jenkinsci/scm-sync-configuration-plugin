097953036f1aa04e2a8f8c15cd63d458261342ac
fcamblor <fcamblor@dev.java.net>
2010-12-08 22:48:10 +0000

    refactored @Before method + provided createSCMMock method, allowing to create a SCM mock for the test execution

--- src/test/java/hudson/plugins/scm_sync_configuration/util/ScmSyncConfigurationBaseTest.java
+++ src/test/java/hudson/plugins/scm_sync_configuration/util/ScmSyncConfigurationBaseTest.java
@@ -1,13 +1,18 @@
 package hudson.plugins.scm_sync_configuration.util;
 
 import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.notNull;
 import static org.powermock.api.easymock.PowerMock.createPartialMock;
 import static org.powermock.api.easymock.PowerMock.mockStatic;
-import static org.powermock.api.easymock.PowerMock.replayAll;
+import static org.powermock.api.easymock.PowerMock.replay;
 import hudson.model.Hudson;
+import hudson.plugins.scm_sync_configuration.scms.SCM;
+import hudson.plugins.scm_sync_configuration.scms.SCMCredentialConfiguration;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.codehaus.plexus.util.FileUtils;
 import org.junit.After;
@@ -18,7 +23,7 @@ import org.powermock.modules.junit4.PowerMockRunner;
 import org.springframework.core.io.ClassPathResource;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest(Hudson.class)
+@PrepareForTest({Hudson.class, SCM.class})
 public class ScmSyncConfigurationBaseTest {
 	
 	private File currentTestDirectory = null;
@@ -26,24 +31,28 @@ public class ScmSyncConfigurationBaseTest {
 
 	@Before
 	public void setup() throws Throwable {
-		// Mocking Hudson singleton instance ...
-		mockStatic(Hudson.class);
-		Hudson hudsonMockedInstance = createPartialMock(Hudson.class, new String[]{ "getRootDir" });
-		expect(Hudson.getInstance()).andStubReturn(hudsonMockedInstance);
-		
 		// Mocking Hudson root directory
 		currentTestDirectory = createTmpDirectory();
 		File tmpHudsonRoot = new File(currentTestDirectory.getAbsolutePath()+"/hudsonRootDir/");
 	    if(!(tmpHudsonRoot.mkdir())) { throw new IOException("Could not create hudson root directory: " + tmpHudsonRoot.getAbsolutePath()); }
-		expect(hudsonMockedInstance.getRootDir()).andStubReturn(tmpHudsonRoot);
 		FileUtils.copyDirectoryStructure(new ClassPathResource("hudsonRootBaseTemplate/").getFile(), tmpHudsonRoot);
-		
+
+        //EnvVars env = Computer.currentComputer().getEnvironment();
+        //env.put("HUDSON_HOME", tmpHudsonRoot.getPath() );
+
 		// Creating local SVN repository...
 		curentLocalSvnRepository = new File(currentTestDirectory.getAbsolutePath()+"/svnLocalRepo/");
 	    if(!(curentLocalSvnRepository.mkdir())) { throw new IOException("Could not create SVN local repo directory: " + curentLocalSvnRepository.getAbsolutePath()); }
 	    FileUtils.copyDirectoryStructure(new ClassPathResource("svnEmptyRepository").getFile(), curentLocalSvnRepository);
-		
-		replayAll();
+
+		// Mocking Hudson singleton instance ...
+		mockStatic(Hudson.class);
+		Hudson hudsonMockedInstance = createPartialMock(Hudson.class, new String[]{ "getRootDir" });
+		expect(Hudson.getInstance()).andStubReturn(hudsonMockedInstance);
+		expect(hudsonMockedInstance.getRootDir()).andStubReturn(tmpHudsonRoot);
+
+		replay(hudsonMockedInstance);
+		replay(Hudson.class);
 	}
 	
 	@After
@@ -58,7 +67,35 @@ public class ScmSyncConfigurationBaseTest {
 	    if(!(temp.mkdir())) { throw new IOException("Could not create temp directory: " + temp.getAbsolutePath()); }
 	    return (temp);
 	}
+	
+	protected SCM createSCMMock(boolean withCredentials){
+		
+		List<String> partiallyMockedMethods = new ArrayList<String>();
+		if(withCredentials){
+			partiallyMockedMethods.add("extractScmCredentials");
+		}
+		
+		SCM mockedSCM = createPartialMock(getSCMClass(), partiallyMockedMethods.toArray(new String[0]));
+		mockStatic(SCM.class);
+		
+		expect(SCM.valueOf(notNull(String.class))).andReturn(mockedSCM);
 
+		if(withCredentials){
+			SCMCredentialConfiguration mockedCredential = new SCMCredentialConfiguration("toto");
+			expect(mockedSCM.extractScmCredentials(notNull(String.class))).andReturn(mockedCredential).anyTimes();
+		}
+		
+		replay(mockedSCM);
+		replay(SCM.class);
+		
+		return mockedSCM;
+	}
+
+	// Overridable in a near future (when dealing for multiple scms ..)
+	protected Class<? extends SCM> getSCMClass(){
+		return SCM.SUBVERSION.getClass();
+	}
+	
 	protected File getCurrentTestDirectory() {
 		return currentTestDirectory;
 	}
