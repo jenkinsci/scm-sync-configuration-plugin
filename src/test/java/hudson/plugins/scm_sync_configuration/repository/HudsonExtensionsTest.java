1440ee4ec843f946f7accf7428f12c66e279c1c1
fcamblor <fcamblor@dev.java.net>
2010-12-13 23:49:59 +0000

    refactored HudsonExtensionsTest to test ScmSyncConfigurationItemListener instead of ScmSyncConfigurationBusiness

--- src/test/java/hudson/plugins/scm_sync_configuration/repository/HudsonExtensionsTest.java
+++ src/test/java/hudson/plugins/scm_sync_configuration/repository/HudsonExtensionsTest.java
@@ -1,24 +1,47 @@
 package hudson.plugins.scm_sync_configuration.repository;
 
-import java.io.File;
-
-import org.junit.Before;
-import org.junit.Test;
-
+import static org.easymock.EasyMock.anyObject;
+import static org.easymock.EasyMock.createMock;
+import static org.easymock.EasyMock.expect;
+import static org.powermock.api.easymock.PowerMock.createPartialMock;
+import static org.powermock.api.easymock.PowerMock.mockStatic;
+import static org.powermock.api.easymock.PowerMock.replay;
+import hudson.model.Item;
+import hudson.model.Saveable;
 import hudson.model.Hudson;
 import hudson.plugins.scm_sync_configuration.ScmSyncConfigurationBusiness;
 import hudson.plugins.scm_sync_configuration.ScmSyncConfigurationPlugin;
+import hudson.plugins.scm_sync_configuration.extensions.ScmSyncConfigurationItemListener;
 import hudson.plugins.scm_sync_configuration.model.ScmContext;
 import hudson.plugins.scm_sync_configuration.scms.SCM;
 import hudson.plugins.scm_sync_configuration.util.ScmSyncConfigurationBaseTest;
 
+import java.io.File;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+
+@PrepareForTest(ScmSyncConfigurationPlugin.class)
 public class HudsonExtensionsTest extends ScmSyncConfigurationBaseTest {
 
 	private ScmSyncConfigurationBusiness sscBusiness;
+	private ScmSyncConfigurationItemListener sscItemListener;
 	
 	@Before
-	public void initBusiness() throws Throwable{
+	public void initObjectsUnderTests() throws Throwable{
 		this.sscBusiness = new ScmSyncConfigurationBusiness();
+		this.sscItemListener = new ScmSyncConfigurationItemListener();
+
+		// Mocking ScmSyncConfigurationPlugin singleton ...
+		mockStatic(ScmSyncConfigurationPlugin.class);
+		ScmSyncConfigurationPlugin mockedPlugin = createPartialMock(ScmSyncConfigurationPlugin.class, new String[]{ "getStrategyForSaveable" });
+		mockedPlugin.setBusiness(this.sscBusiness);
+		expect(ScmSyncConfigurationPlugin.getInstance()).andStubReturn(mockedPlugin);
+		expect(mockedPlugin.getStrategyForSaveable(anyObject(Saveable.class), anyObject(File.class))).andStubReturn(ScmSyncConfigurationPlugin.AVAILABLE_STRATEGIES[0]);
+		
+		replay(mockedPlugin);
+		replay(ScmSyncConfigurationPlugin.class);
 	}
 	
 	@Test
@@ -32,11 +55,15 @@ public class HudsonExtensionsTest extends ScmSyncConfigurationBaseTest {
 		sscBusiness.synchronizeAllConfigs(scmContext, ScmSyncConfigurationPlugin.AVAILABLE_STRATEGIES, Hudson.getInstance().getMe());
 		
 		// Renaming fakeJob to newFakeJob
-		File oldDir = new File(getCurrentHudsonRootDirectory() + File.separator + "jobs/fakeJob/" );
-		File newDir = new File(getCurrentHudsonRootDirectory() + File.separator + "jobs/newFakeJob/" );
-		sscBusiness.renameHierarchy(scmContext, oldDir, newDir, Hudson.getInstance().getMe());
+		Item mockedItem = createMock(Item.class);
+		File mockedItemRootDir = new File(getCurrentHudsonRootDirectory() + "/jobs/newFakeJob/" );
+		expect(mockedItem.getRootDir()).andStubReturn(mockedItemRootDir);
+		replay(mockedItem);
+		
+		sscItemListener.onRenamed(mockedItem, "fakeJob", "newFakeJob");
 		
 		verifyCurrentScmContentMatchesHierarchy("expected-scm-hierarchies/HudsonExtensionsTest.shouldJobRenameBeCorrectlyImpactedOnSCM/");
 	}
+	
 	
 }
