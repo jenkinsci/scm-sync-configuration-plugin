4b210fcdeb1117f73f919c784cd130e52055d746
fcamblor <fcamblor@gmail.com>
2011-03-02 02:43:35 +0100

    declared sisu instead of default-plexus-container dependency in pom.xml + used DefaultPlexusContainer class in ScmManagerFactory instead of Embedder It doesn't work on Hudson 1.339 because of some strange classloader issue (core's default-plexus-container takes precedence over plugin's sisu-inject-plexus :( )

--- src/main/java/hudson/plugins/scm_sync_configuration/SCMManagerFactory.java
+++ src/main/java/hudson/plugins/scm_sync_configuration/SCMManagerFactory.java
@@ -1,23 +1,24 @@
 package hudson.plugins.scm_sync_configuration;
 
 import org.apache.maven.scm.manager.ScmManager;
+import org.codehaus.plexus.DefaultPlexusContainer;
+import org.codehaus.plexus.PlexusContainer;
 import org.codehaus.plexus.PlexusContainerException;
 import org.codehaus.plexus.component.repository.exception.ComponentLookupException;
-import org.codehaus.plexus.embed.Embedder;
 
 public class SCMManagerFactory {
 
 	private static final SCMManagerFactory INSTANCE = new SCMManagerFactory();
 	
-	private Embedder plexus = null;
+	private PlexusContainer plexus = null;
 
 	private SCMManagerFactory(){
 	}
 	
 	public void start() throws PlexusContainerException {
 		if(plexus == null){
-			this.plexus = new Embedder();
-			this.plexus.start();
+			this.plexus = new DefaultPlexusContainer();
+			//this.plexus.start();
 		}
 	}
 	
@@ -26,7 +27,7 @@ public class SCMManagerFactory {
 	}
 	
 	public void stop() throws Exception {
-		this.plexus.stop();
+		this.plexus.dispose();
 		this.plexus = null;
 	}
 	
