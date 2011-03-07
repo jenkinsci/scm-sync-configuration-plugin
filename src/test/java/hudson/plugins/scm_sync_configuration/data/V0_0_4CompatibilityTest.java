7efb62dc5db48dd39558cf671387276948e2124d
fcamblor <fcamblor@gmail.com>
2011-03-07 02:12:46 +0100

    provided v0.0.4 empty scm-sync-configuration file test

---
+++ src/test/java/hudson/plugins/scm_sync_configuration/data/V0_0_4CompatibilityTest.java
@@ -0,0 +1,25 @@
+package hudson.plugins.scm_sync_configuration.data;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+import hudson.plugins.scm_sync_configuration.ScmSyncConfigurationPlugin;
+import hudson.plugins.scm_sync_configuration.scms.ScmSyncNoSCM;
+import hudson.plugins.scm_sync_configuration.util.ScmSyncConfigurationPluginBaseTest;
+
+import org.junit.Test;
+
+public class V0_0_4CompatibilityTest extends ScmSyncConfigurationPluginBaseTest {
+
+	protected String getHudsonRootBaseTemplate() {
+		return "hudsonRoot0.0.4WithEmptyConfTemplate/";
+	}
+	
+	@Test
+	public void should0_0_4_pluginEmptyConfigurationFileShouldLoadCorrectly() throws Throwable {
+		ScmSyncConfigurationPlugin plugin = ScmSyncConfigurationPlugin.getInstance();
+		assertThat(plugin.getSCM(), is(notNullValue()));
+		assertThat(plugin.getSCM().getId(), is(equalTo(ScmSyncNoSCM.class.getName())));
+	}
+}
