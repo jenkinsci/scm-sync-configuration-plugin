5795ca558912fa9a84c3bf7594e3f0937b4b7d4f
fcamblor <fcamblor@gmail.com>
2011-03-07 02:12:16 +0100

    provided test with empty scm-sync-congiguration data

--- src/test/java/hudson/plugins/scm_sync_configuration/data/V0_0_2CompatibilityTest.java
+++ src/test/java/hudson/plugins/scm_sync_configuration/data/V0_0_2CompatibilityTest.java
@@ -5,7 +5,8 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
 import hudson.plugins.scm_sync_configuration.ScmSyncConfigurationPlugin;
-import hudson.plugins.scm_sync_configuration.scms.impl.ScmSyncSubversionSCM;
+import hudson.plugins.scm_sync_configuration.scms.ScmSyncNoSCM;
+import hudson.plugins.scm_sync_configuration.scms.ScmSyncSubversionSCM;
 import hudson.plugins.scm_sync_configuration.util.ScmSyncConfigurationPluginBaseTest;
 
 import org.junit.Test;
@@ -13,7 +14,13 @@ import org.junit.Test;
 public class V0_0_2CompatibilityTest extends ScmSyncConfigurationPluginBaseTest {
 
 	protected String getHudsonRootBaseTemplate() {
-		return "hudsonRoot0.0.2BaseTemplate/";
+		if("should0_0_2_pluginConfigurationFileShouldLoadCorrectly".equals(testName.getMethodName())){
+			return "hudsonRoot0.0.2BaseTemplate/";
+		} else if("should0_0_2_pluginEmptyConfigurationFileShouldLoadCorrectly".equals(testName.getMethodName())){
+			return "hudsonRoot0.0.2WithEmptyConfTemplate/";
+		} else {
+			throw new IllegalArgumentException("Unsupported test name : "+testName);
+		}
 	}
 	
 	@Test
@@ -22,5 +29,12 @@ public class V0_0_2CompatibilityTest extends ScmSyncConfigurationPluginBaseTest 
 		ScmSyncConfigurationPlugin plugin = ScmSyncConfigurationPlugin.getInstance();
 		assertThat(plugin.getSCM(), is(notNullValue()));
 		assertThat(plugin.getSCM().getId(), is(equalTo(ScmSyncSubversionSCM.class.getName())));
+	}
+	
+	@Test
+	public void should0_0_2_pluginEmptyConfigurationFileShouldLoadCorrectly() throws Throwable {
+		ScmSyncConfigurationPlugin plugin = ScmSyncConfigurationPlugin.getInstance();
+		assertThat(plugin.getSCM(), is(notNullValue()));
+		assertThat(plugin.getSCM().getId(), is(equalTo(ScmSyncNoSCM.class.getName())));
 	}
 }
