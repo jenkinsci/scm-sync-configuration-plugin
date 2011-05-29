fc5001cdc4c304738555d53acb0bca4f0299d920
Frederic Camblor <fcamblor@gmail.com>
2011-05-29 08:31:05 +0200

    provided some comments

--- src/main/java/hudson/plugins/scm_sync_configuration/strategies/impl/JobConfigScmSyncStrategy.java
+++ src/main/java/hudson/plugins/scm_sync_configuration/strategies/impl/JobConfigScmSyncStrategy.java
@@ -13,7 +13,11 @@ import java.util.List;
 
 public class JobConfigScmSyncStrategy extends AbstractScmSyncStrategy {
 
+    // Don't miss to take into account view urls since we can configure a job through a view !
 	private static final List<PageMatcher> PAGE_MATCHERS = new ArrayList<PageMatcher>(){ { add(new PageMatcher("^(.*view/[^/]+/)?job/[^/]+/configure$", "config")); } };
+    // Only saving config.xml file located in job directory
+    // Some plugins (like maven release plugin) could add their own configuration files in the job directory that we don't want to synchronize
+    // ... at least in the current strategy !
 	private static final ConfigurationEntityMatcher CONFIG_ENTITY_MANAGER = new ClassAndFileConfigurationEntityMatcher(Job.class, "^jobs/[^/]+/config\\.xml$");
 	
 	public JobConfigScmSyncStrategy(){
