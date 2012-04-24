21eff73b7700806b6daa197cfc212ee72f4997db
Bertrand Paquet <bpaquet@octo.com>
2012-04-24 16:50:05 +0200

    Add git implementation

--- src/main/java/hudson/plugins/scm_sync_configuration/scms/SCM.java
+++ src/main/java/hudson/plugins/scm_sync_configuration/scms/SCM.java
@@ -24,6 +24,7 @@ public abstract class SCM {
 	protected static final List<SCM> SCM_IMPLEMENTATIONS = new ArrayList<SCM>(){ {
 		add(new ScmSyncNoSCM());
 		add(new ScmSyncSubversionSCM());
+		add(new ScmSyncGitSCM());
 	} };
 	
 	transient protected String title;
