3c20fd081f19a0a6263777eb11ba922fdcca403e
Clarence Bakirtzidis <clarenceb@gmail.com>
2012-12-10 14:20:13 +1100

    HG Support: Removed the 'touch' command from test and used 'FileUtils.fileWrite' instead so its not *nix specific.

--- src/test/java/hudson/plugins/test/utils/scms/ScmUnderTestHg.java
+++ src/test/java/hudson/plugins/test/utils/scms/ScmUnderTestHg.java
@@ -5,17 +5,24 @@ import hudson.plugins.scm_sync_configuration.scms.ScmSyncHgSCM;
 
 import java.io.File;
 
+import org.codehaus.plexus.util.FileUtils;
+
 public class ScmUnderTestHg implements ScmUnderTest {
 
 	public void initRepo(File path) throws Exception {
+	    // HG support: Unlike Git, Mercurial does not appear to support "bare" repositories.
+		// So we have to initialise the repository by committing a dummy file.
+
+		final String dummyFileName = "hg_dummy.txt";
+		File dummyFile = new File(path, dummyFileName);
+		FileUtils.fileWrite(dummyFile.getAbsolutePath(), "This is a dummy file");
+
 		ProcessBuilder[] commands = new ProcessBuilder[] {
-		    // HG support: Unlike Git, Hg does not appear to support "bare" repos.
-			// So we have to initialise the repo by commiting a dummy file.
 			new ProcessBuilder("hg", "init"),
-		    new ProcessBuilder("touch", "hg_dummy.txt"),
-		    new ProcessBuilder("hg", "add", "hg_dummy.txt"),
+		    new ProcessBuilder("hg", "add", dummyFileName),
 		    new ProcessBuilder("hg", "commit", "-m", "\"initial commit\"")
 		};
+
 		for (ProcessBuilder pb : commands) {
 			pb.directory(path);
 			Process p = pb.start();
