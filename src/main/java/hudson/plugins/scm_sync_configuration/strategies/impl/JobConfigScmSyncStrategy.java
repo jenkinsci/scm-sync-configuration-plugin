141cc2b5bfefdf84c603eb0c49803de64629eb93
Nicolas De Loof <nicolas.deloof@gmail.com>
2014-02-04 12:09:48 +0100

    Support folders (cloudbees folders plugin) Consider nested level of '/jobs/*' as candidate files for sync don't assume all items are actual hudson.model.Job, prefer TopLevelItem

--- src/main/java/hudson/plugins/scm_sync_configuration/strategies/impl/JobConfigScmSyncStrategy.java
+++ src/main/java/hudson/plugins/scm_sync_configuration/strategies/impl/JobConfigScmSyncStrategy.java
@@ -4,6 +4,7 @@ import hudson.XmlFile;
 import hudson.model.Item;
 import hudson.model.Job;
 import hudson.model.Saveable;
+import hudson.model.TopLevelItem;
 import hudson.plugins.scm_sync_configuration.model.MessageWeight;
 import hudson.plugins.scm_sync_configuration.model.WeightedMessage;
 import hudson.plugins.scm_sync_configuration.strategies.AbstractScmSyncStrategy;
@@ -18,15 +19,15 @@ public class JobConfigScmSyncStrategy extends AbstractScmSyncStrategy {
 
     // Don't miss to take into account view urls since we can configure a job through a view !
 	private static final List<PageMatcher> PAGE_MATCHERS = new ArrayList<PageMatcher>(){ {
-        add(new PageMatcher("^(.*view/[^/]+/)?job/[^/]+/configure$", "form[name='config']"));
+        add(new PageMatcher("^(.*view/[^/]+/)?(/job/[^/])*/job/[^/]+/configure$", "form[name='config']"));
     } };
     // Only saving config.xml file located in job directory
     // Some plugins (like maven release plugin) could add their own configuration files in the job directory that we don't want to synchronize
     // ... at least in the current strategy !
 	private static final String [] PATTERNS = new String[] {
-        "jobs/*/config.xml"
+        "**/jobs/*/config.xml"
 	};
-	private static final ConfigurationEntityMatcher CONFIG_ENTITY_MANAGER = new ClassAndFileConfigurationEntityMatcher(Job.class, PATTERNS);
+	private static final ConfigurationEntityMatcher CONFIG_ENTITY_MANAGER = new ClassAndFileConfigurationEntityMatcher(TopLevelItem.class, PATTERNS);
 	
 	public JobConfigScmSyncStrategy(){
 		super(CONFIG_ENTITY_MANAGER, PAGE_MATCHERS);
@@ -35,7 +36,7 @@ public class JobConfigScmSyncStrategy extends AbstractScmSyncStrategy {
     public CommitMessageFactory getCommitMessageFactory(){
         return new CommitMessageFactory(){
             public WeightedMessage getMessageWhenSaveableUpdated(Saveable s, XmlFile file) {
-                return new WeightedMessage("Job ["+((Job)s).getName()+"] configuration updated",
+                return new WeightedMessage("Job ["+((Item)s).getName()+"] configuration updated",
                         // Job config update message should be considered as "important", especially
                         // more important than the plugin descriptors Saveable updates
                         MessageWeight.IMPORTANT);
